2c76c9724a87b4ebe899da23ca287b8105567c96
Robyobon <Robyobon@gmail.com>
2023-08-20 15:43:52 +0700

    perbaikan

--- src/App.jsx
+++ src/App.jsx
@@ -37,12 +37,12 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/home" element={<Home />} />
-          <Route path="/kawula" element={<Kawula />} />
-          <Route path="/gamestore" element={<Gamestore />} />
-          <Route path="/sonokeling" element={<Sonokeling />} />
-          <Route path="/rh" element={<Rh />} />
-          <Route path="/palmbeach" element={<Palmbeach />} />
-          <Route path="/herb" element={<Herb />} />
+          <Route path="/home/kawula" element={<Kawula />} />
+          <Route path="/home/gamestore" element={<Gamestore />} />
+          <Route path="/home/sonokeling" element={<Sonokeling />} />
+          <Route path="/home/rh" element={<Rh />} />
+          <Route path="/home/palmbeach" element={<Palmbeach />} />
+          <Route path="/home/herb" element={<Herb />} />
         </Routes>
         {/* <Footer /> */}
       </motion.div>
