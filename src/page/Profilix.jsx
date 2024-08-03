71e44687f56dd02600d6c613ec32b1bfd4a05199
Robyobon <Robyobon@gmail.com>
2024-08-03 05:31:32 -0700

    Memperbaiki secondlify dan profilix

--- src/page/Profilix.jsx
+++ src/page/Profilix.jsx
@@ -1,19 +1,38 @@
-import Foto1 from '../assets/Profilix.svg'
+import Banner from '../assets/COVER (2).svg'
+import Gambar1 from '../assets/Profilix/Roby (1) 1.svg'
+import Gambar2 from '../assets/Profilix/Mac Studio.svg'
+import Gambar3 from '../assets/Profilix/Group 1000003989.svg'
+import Gambar4 from '../assets/Profilix/MacBook Pro and iPhone 15 Pro Mockup.svg'
+import Gambar5 from '../assets/Profilix/Group 1000003990.svg'
+import Gambar6 from '../assets/Profilix/Group 1000003991.svg'
+import Gambar7 from '../assets/Profilix/Group 1000003992.svg'
 
-const Secondlify = () => {
+const Profilix = () => {
   return (
-    <section className='overflow-hidden profilix'>
-      <div className='w-screen relative '>
-        <div className="flex justify-center">
-          <div className=' smartphone:px-0 smartphone:mx-10'>
-            <div className='gap-y-10 grid grid-cols-1  smartphone:w-[300px]'>
-              <img className='relative bottom-[110px]'src={Foto1} />
+    <section className='overflow-hidden profilix bg-zinc-400'>
+        <div className='w-screen relative'>
+          <div className="flex justify-center">
+            <img src={Banner} alt="banner" className='desktop:h-[100vh] w-full' />
+          </div>
+          <div className='px-[100px] mx-[100px] smartphone:px-0 smartphone:mx-10'>
+            <div className='text-white'>
+              <h1 className='text-[40px] text-center pt-20 smartphone:text-2xl'>Thema Profilix</h1>
+              <article className='text-xl pt-10 smartphone:text-sm'>
+                Portofolio Tema untuk CMS Profilix Oxinos
+                Saya telah mengembangkan sebuah tema portofolio untuk CMS Profilix di Oxinos. Tema ini dirancang untuk memberikan tampilan yang profesional dan menarik, memudahkan pengguna dalam membuat portofolio yang elegan dan fungsional. Hasil kerja ini mencerminkan kemampuan saya dalam menciptakan desain web yang responsif dan modern, memastikan pengalaman pengguna yang mulus di berbagai perangkat.
+              </article>
+              <img src={Gambar1} alt="gambar research" className='pt-6' />
+              <img src={Gambar2} alt="gambar Userflow" />
+              <img src={Gambar3} alt="gambar Userflow" className='relative bottom-96' />
+              <img src={Gambar4} alt="gambar Wireframe 1" className='relative bottom-[540px]' />
+              <img src={Gambar5} alt="gambar Userflow" className='relative bottom-[740px]' />
+              <img src={Gambar6} alt="gambar Userflow" className='relative bottom-[740px]' />
+              <img src={Gambar7} alt="gambar Userflow" className='relative bottom-[740px]' />
             </div>
           </div>
         </div>
-      </div>
     </section>
   )
 }
 
-export default Secondlify
+export default Profilix
--- src/page/Secondlify.jsx
+++ src/page/Secondlify.jsx
@@ -1,17 +1,57 @@
-import Foto1 from '../assets/Secondlify.svg'
+import Banner from '../assets/Secondlify/COVER (4).svg'
+import Research from '../assets/Secondlify/research.svg'
+import Taskflow from '../assets/Secondlify/Task Flow 1.svg'
+import UserFlowuser from '../assets/Secondlify/User Flow 1.svg'
+import UserFlowuser2 from '../assets/Secondlify/User Flow Penjual 1.svg'
+import UserFlowpenawaran from '../assets/Secondlify/User Flow penjual penawaran 1.svg'
+import Wireframe1 from '../assets/Secondlify/Wireframe 1.svg'
+import Wireframe2 from '../assets/Secondlify/Flow user 1.svg'
+import Wireframe3 from '../assets/Secondlify/Wireframe1.svg'
+import Designsystem from '../assets/Secondlify/Designsystem.svg'
+import Hifi from '../assets/Secondlify/Wireframe2.svg'
+import contoh from '../assets/Secondlify/Halaman Awal Sebelum Login.svg'
 
 const Secondlify = () => {
   return (
-    <section className='overflow-hidden secondlify'>
-      <div className='w-screen relative '>
-        <div className="flex justify-center">
-          <div className=' smartphone:px-0 smartphone:mx-10'>
-            <div className='gap-y-10 grid grid-cols-1  smartphone:w-[300px]'>
-              <img className='relative bottom-[110px]'src={Foto1} />
+    <section className='overflow-hidden gamestore'>
+        <div className='w-screen relative'>
+          <div className="flex justify-center">
+            <img src={Banner} alt="banner" className='desktop:h-[100vh] w-full' />
+          </div>
+          <div className='px-[100px] mx-[100px] smartphone:px-0 smartphone:mx-10'>
+            <div className='text-white'>
+            <h1 className='text-[40px] text-center pt-20 smartphone:text-2xl'>Secondlify</h1>
+            <article className='text-xl pt-10 smartphone:text-sm'>
+           Secondlify adalah platform jual beli barang bekas yang menghadirkan kemudahan bagi pengguna untuk menjual dan membeli barang bekas berkualitas. Dengan fitur unggulan pengecekan kualitas barang dan layanan pengiriman yang terpercaya, Secondlify memberikan pengalaman berbelanja online yang aman dan nyaman bagi setiap penggunanya. Temukan berbagai pilihan barang bekas dari elektronik, pakaian, furnitur, hingga perlengkapan rumah tangga dengan harga yang kompetitif dan kualitas terjamin. Jual barang bekas Anda dengan mudah atau temukan barang bekas yang Anda cari hanya di Secondlify, tempatnya belanja pintar untuk masa depan yang lebih berkelanjutan.
+            </article>
+              <h2 className='text-[32px] pt-10'>Process</h2>
+              <h3 className='text-[24px] pb-[40px]'>Research</h3>
+              <img src={Research} alt="gambar research" />
+              <h3 className='text-[32px] pt-[40px] pb-[40px]'>User Flow & Task Flow</h3>
+              
+              <h4 className='text-[24px] pb-[40px]'>User Flow User</h4>
+              
+              <img src={UserFlowuser} alt="gambar Userflow" />
+              <img src={UserFlowuser2} alt="gambar Userflow" />
+              <h4 className='text-[24px] pb-[40px] pt-10'>User Flow Penawaran</h4>
+              
+              <img src={UserFlowpenawaran} alt="gambar Userflow" />
+
+              <h3 className='text-[24px] pt-[40px] pb-[40px]'>Task Flow</h3>
+              <img src={Taskflow} alt="gambar Task" />
+              <h3 className='text-[24px] pt-[40px] pb-[40px]'>Wireframe & Wireframeflow</h3>
+              <img src={Wireframe1} alt="gambar Wireframe 1" />
+              <img src={Wireframe2} alt="gambar Wireframe 2" />
+              <img src={Wireframe3} alt="gambar Wireframe 2" />
+              <img src={Designsystem} alt="gambar Mockup 1" />
+               <h3 className='text-[24px] pb-[40px] pt-12'>Hifi</h3>
+              <img src={Hifi} alt="gambar research" />
+           
+              <img src={contoh} alt="gambar research" />
+           
             </div>
           </div>
         </div>
-      </div>
     </section>
   )
 }
