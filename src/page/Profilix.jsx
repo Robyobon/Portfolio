ddc11d61d9cb4d70ed2aedb5571e5562cadeeb92
Robyobon <Robyobon@gmail.com>
2024-08-03 05:59:56 -0700

    layouting

--- src/page/Profilix.jsx
+++ src/page/Profilix.jsx
@@ -9,25 +9,25 @@ import Gambar7 from '../assets/Profilix/Group 1000003992.svg'
 
 const Profilix = () => {
   return (
-    <section className='overflow-hidden profilix bg-zinc-400'>
+    <section className='overflow-hidden profilix bg-white'>
         <div className='w-screen relative'>
           <div className="flex justify-center">
             <img src={Banner} alt="banner" className='desktop:h-[100vh] w-full' />
           </div>
           <div className='px-[100px] mx-[100px] smartphone:px-0 smartphone:mx-10'>
-            <div className='text-white'>
+            <div className='text-black'>
               <h1 className='text-[40px] text-center pt-20 smartphone:text-2xl'>Thema Profilix</h1>
               <article className='text-xl pt-10 smartphone:text-sm'>
                 Portofolio Tema untuk CMS Profilix Oxinos
                 Saya telah mengembangkan sebuah tema portofolio untuk CMS Profilix di Oxinos. Tema ini dirancang untuk memberikan tampilan yang profesional dan menarik, memudahkan pengguna dalam membuat portofolio yang elegan dan fungsional. Hasil kerja ini mencerminkan kemampuan saya dalam menciptakan desain web yang responsif dan modern, memastikan pengalaman pengguna yang mulus di berbagai perangkat.
               </article>
               <img src={Gambar1} alt="gambar research" className='pt-6' />
-              <img src={Gambar2} alt="gambar Userflow" />
-              <img src={Gambar3} alt="gambar Userflow" className='relative bottom-96' />
-              <img src={Gambar4} alt="gambar Wireframe 1" className='relative bottom-[540px]' />
-              <img src={Gambar5} alt="gambar Userflow" className='relative bottom-[740px]' />
-              <img src={Gambar6} alt="gambar Userflow" className='relative bottom-[740px]' />
-              <img src={Gambar7} alt="gambar Userflow" className='relative bottom-[740px]' />
+              <img src={Gambar2} alt="gambar Userflow" className='' />
+              <img src={Gambar3} alt="gambar Userflow" className='relative bottom-96 smartphone:bottom-16' />
+              <img src={Gambar4} alt="gambar Wireframe 1" className='relative bottom-[540px] smartphone:bottom-16' />
+              <img src={Gambar5} alt="gambar Userflow" className='relative bottom-[740px] smartphone:bottom-16' />
+              <img src={Gambar6} alt="gambar Userflow" className='relative bottom-[740px] smartphone:bottom-16' />
+              <img src={Gambar7} alt="gambar Userflow" className='relative bottom-[740px] smartphone:bottom-16' />
             </div>
           </div>
         </div>
